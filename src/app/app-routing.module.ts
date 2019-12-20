6bd84b83d426c801e9016a5f1059e6214f6400af
Pedro Reynoso Ponciano <pedror@senalcorporativo.com>
2019-12-20 13:40:57 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {path: '', redirectTo: 'auth', pathMatch: 'full'},
   {path: 'auth', loadChildren: '@auth/auth.module#AuthModule'},
+  {path: 'dashboard', loadChildren: '@dashboard/dashboard.module#DashboardModule'},
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login/login.component';
 import {AuthRoutingModule} from './auth-routing.module';
 import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import {FormsModule} from '@angular/forms';
     CommonModule,
     FormsModule,
     AuthRoutingModule,
+    SharedModule,
   ]
 })
 export class AuthModule {
