72034b7be831ae1790e19ac6c70e7eaa67d8835a
MauricioLoya <mauloy15@gmail.com>
2019-12-21 10:17:06 -0600

    [UPDATE] barra de navagacion e interactividad con imagenes

--- src/app/dactiloscapia/dactiloscapia.component.ts
+++ src/app/dactiloscapia/dactiloscapia.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dactiloscapia.component.scss']
 })
 export class DactiloscapiaComponent implements OnInit {
+  public isLoadingImages: boolean;
+  public isLoadingDactiloscopia: boolean;
+  public uriNoImage: string
+  public iconLoading: string;
 
-  constructor() { }
+  constructor() {
+    this.isLoadingImages = true;
+    this.uriNoImage = 'no_image';
+    this.isLoadingDactiloscopia = true;
+    this.iconLoading = '<i class="fa fa-spinner fa-spin" aria-hidden="true"></i>';
+  }
 
   ngOnInit() {
+  }
+
+  showImages = () => {
+    setTimeout(() => {
+      this.isLoadingImages = false;
+      }, 700);
+  }
+
+  showDatiloscopia = () => {
+    setTimeout(() => {
+      this.isLoadingDactiloscopia = false;
+    }, 700);
   }
 
 }
