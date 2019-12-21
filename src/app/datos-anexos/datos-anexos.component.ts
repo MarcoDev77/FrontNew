36fc8299634a3c7c0d022150c2bfb54d8f01779f
Carlos Gutierrez Martinez <carlosg@senalcorporativo.com>
2019-12-21 11:48:49 -0600

    [UPDATE] paso de parametros

--- src/app/datos-anexos/datos-anexos.component.ts
+++ src/app/datos-anexos/datos-anexos.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-datos-anexos',
   templateUrl: './datos-anexos.component.html',
   styleUrls: ['./datos-anexos.component.scss']
 })
 export class DatosAnexosComponent implements OnInit {
-
-  constructor() { }
+  public ingreso: any ={};
+  constructor(public router: Router) { }
 
   ngOnInit() {
-  }
+    this.ingreso = JSON.parse(localStorage.getItem('ingreso'));
 
+  }
+  submit(){
+    this.router.navigate(['/dashboard/dactiloscopia/'])
+  }
 }
--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-ingreso',
@@ -7,14 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoComponent implements OnInit {
   public ingreso:any = {};
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
 
   submit(){
     console.log("entra al metodo")
+    localStorage.setItem("ingreso",JSON.stringify(this.ingreso));
     console.log (this.ingreso);
+    this.router.navigate(['/dashboard/datosAnexos/'])
   }
 
 }
