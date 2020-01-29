7fb534843603f80c57a2f1970cddeaa669313c47
Carlos Gutiérrez <carlosg@senalcorporativo.com>
2020-01-29 09:41:52 -0600

    [ADD] pase unico

--- src/app/dashboard/servicio-social/servicio-social.module.ts
+++ src/app/dashboard/servicio-social/servicio-social.module.ts
@@ -4,13 +4,17 @@ import { PaseUnicoComponent } from './pase-unico/pase-unico.component';
 import { PaseMensualComponent } from './pase-mensual/pase-mensual.component';
 import { RootComponent } from './_root/root.component';
 import { ServicioSocialRoutingModule } from './servicio.social.routing.module';
+import { SharedModule } from '@shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [PaseUnicoComponent, PaseMensualComponent, RootComponent],
   imports: [
-    CommonModule, ServicioSocialRoutingModule
+    CommonModule,
+    SharedModule,
+    FormsModule, ServicioSocialRoutingModule
   ]
 })
 export class ServicioSocialModule { }
