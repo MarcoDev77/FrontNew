fbc201fd2f0e03535b32163e4f385974e8772a18
Mauricio Loya <mauloy15@gmail.com>
2020-02-05 12:19:59 -0600

    [UPDATE] mapeando las dos imagenes del cuerpo

--- src/app/dashboard/ingreso/caracteristicas/caracteristicas.component.ts
+++ src/app/dashboard/ingreso/caracteristicas/caracteristicas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-caracteristicas',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaracteristicasComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $('image-map').imageMapResize();
+  }
 
   ngOnInit() {
   }
+
 
 }
--- src/app/typings.d.ts
+++ src/app/typings.d.ts
@@ -1,3 +1,4 @@
 declare var tinymce: any;
 declare var swal: any;
 declare var ol: any;
+declare var $: any;
