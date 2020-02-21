bac072ab01299fa338d2774f8cced683b3000ebb
carlos <i20163ti010@utez.edu.mx>
2020-02-21 13:09:46 -0600

    [FIX] usuarios, limpieza

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,27 +4,14 @@ import {AuthGuard} from '@shared/helpers/auth.guard';
 import {roles as r} from '../shared/helpers/roles';
 // App
 import {MainComponent} from './_main/main.component';
-import {DatosAnexosComponent} from '../datos-anexos/datos-anexos.component';
-import {MediaFiliacionComponent} from '@dashboard/media-filiacion/media-filiacion.component';
-import {IngresoComponent} from '../ingreso/ingreso.component';
-import {DactiloscapiaComponent} from '../dactiloscapia/dactiloscapia.component';
+
+
+
 
 const routes: Routes = [
   {
     path: '', component: MainComponent,
     children: [
-      {
-        path: 'mediafiliacion', component: MediaFiliacionComponent, data: {expectedRole: [r.test.role]}
-      },
-      {
-        path: 'datosAnexos', component: DatosAnexosComponent, canActivate: [AuthGuard], data: {expectedRole: [r.admin.role]}
-      },
-      {
-        path: 'ingreso2', component: IngresoComponent, canActivate: [AuthGuard], data: {expectedRole: [r.test.role]}
-      },
-      {
-        path: 'dactiloscopia', component: DactiloscapiaComponent, canActivate: [AuthGuard], data: {expectedRole: [r.test.role]}
-      },
       {
         path: 'catalogo',
         loadChildren: './catalogos/catalogos.module#CatalogosModule',
