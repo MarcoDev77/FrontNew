72cd9444d1275d1b3da9a664198451547b2564ea
Carlos Gutierrez <carlosgr822@gmail.com>
2020-04-18 10:04:13 -0500

    [UPDATE] PDF psicologo funcional

--- src/app/dashboard/comite-tecnico/psicologia/psicologia.component.ts
+++ src/app/dashboard/comite-tecnico/psicologia/psicologia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComiteTecnicoService } from '@shared/services/comite-tecnico.service';
 import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-psicologia',
@@ -10,7 +11,8 @@ import Swal from 'sweetalert2';
 export class PsicologiaComponent implements OnInit {
   public generalidadesPPL: GeneralidadesPPL;
   public isLoading: boolean;
-  constructor(private comiteTecnicoService: ComiteTecnicoService) {
+  public file:any;
+  constructor(private comiteTecnicoService: ComiteTecnicoService, private modalService: NgbModal) {
     this.generalidadesPPL= {} as any;
     this.generalidadesPPL.ficha= {} as any;
     this.isLoading=false;
@@ -23,7 +25,16 @@ export class PsicologiaComponent implements OnInit {
   searchImputado(){
     this.comiteTecnicoService.getImputadoByFolioPsicologia(this.generalidadesPPL.folio).subscribe((data:any)=>{
       console.log("getData",data)
-      this.generalidadesPPL=data.imputado
+      if(!data.error){
+        this.generalidadesPPL=data.imputado
+      }
+      Swal.fire({
+        title: data.error ? 'Error!' : 'Busqueda',
+        text: data.mensaje,
+        icon: data.error ? 'error' : 'success',
+        timer: 1000,
+        showConfirmButton: false
+      });
     })
   }
 
@@ -43,6 +54,48 @@ export class PsicologiaComponent implements OnInit {
     })
   }
   
+  generatePDF(modal) {
+    console.log('generatePDF');
+    this.comiteTecnicoService.generatePDFPsicologia(this.generalidadesPPL.imputadoId).subscribe((data: any) => {
+      console.log('PDF', data);
+      this.isLoading = false;
+      const file = new Blob([data], {type: 'application/*'});
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadstart = ev => this.isLoading = true;
+      reader.onloadend = () => {
+        this.isLoading = false;
+        let dataUrl: any;
+        dataUrl = reader.result;
+        const base64 = dataUrl.split(',')[1];
+        this.modalService.dismissAll();
+        if (base64) {
+          this.file = base64;
+          this.modalService.open(modal, {size: 'lg', windowClass: 'modal-primary'});
+        }
+      };
+      reader.onerror = () => {
+        this.isLoading = false;
+        Swal.fire({
+          title: 'Error',
+          text: 'Error al generar el archivo',
+          icon: 'error',
+          timer: 1500,
+          showConfirmButton: false
+        });
+        this.modalService.dismissAll();
+      };
+    }, error => {
+      console.log(error);
+      Swal.fire({
+        title: 'Error',
+        text: 'Error al generar el archivo',
+        icon: 'error',
+        timer: 1500,
+        showConfirmButton: false
+      });
+    });
+  }
 
 }
 
@@ -50,7 +103,7 @@ class GeneralidadesPPL {
   imputadoId: number;
   folio: string;
   nombre: string;
-  dormitorio: any;
+  dormitorio: string;
   fechaNacimiento: Date;
   edad: number;
   estadoCivil: any;
