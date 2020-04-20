2b533d951677b992d7ff9b84a0b7d42ad8d0f634
Carlos Gutierrez <carlosgr822@gmail.com>
2020-04-20 14:00:42 -0500

    [UPD] Menu comite tecnico

---
+++ src/app/dashboard/comite-tecnico/reporte-medico/reporte-medico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-medico',
+  templateUrl: './reporte-medico.component.html',
+  styleUrls: ['./reporte-medico.component.scss']
+})
+export class ReporteMedicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
