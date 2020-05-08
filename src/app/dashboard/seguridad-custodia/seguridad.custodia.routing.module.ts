2de1f3567b484c08b443a9d2495b0cbfcf6e632d
MauricioLoya <mauloy15@hotmail.com>
2020-05-08 09:52:32 -0500

    [ADD] Pantalla de capacitacioes

--- src/app/dashboard/seguridad-custodia/seguridad.custodia.routing.module.ts
+++ src/app/dashboard/seguridad-custodia/seguridad.custodia.routing.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RootComponent} from '@dashboard/seguridad-custodia/_root/root.component';
+import { CapacitacionesComponent } from './capacitaciones/capacitaciones.component';
 
 const routes: Routes = [
   {
     path: '', component: RootComponent,
     children: [
-      {path: '', redirectTo: 'pase-mensual', pathMatch: 'full'},
-      
+      {path: '', redirectTo: 'capacitaciones', pathMatch: 'full'},
+      {path: 'capacitaciones', component: CapacitacionesComponent},
     ]
   },
 ];
