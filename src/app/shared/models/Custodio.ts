c6749d755d0b7ca3cf78942e9b32ce87c83c87e1
MauricioLoya <mauloy15@hotmail.com>
2020-05-09 13:04:20 -0500

    [UPDATE] Registro y eliminacion de custodios

---
+++ src/app/shared/models/Custodio.ts
@@ -0,0 +1,13 @@
+export class Custodio {
+  id: number;
+  numCustodio: string;
+  nombre: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+  area: string;
+  turno: string;
+  horario: string;
+  curp: string;
+  formacionInicial: boolean;
+  nombramiento: any;
+}
