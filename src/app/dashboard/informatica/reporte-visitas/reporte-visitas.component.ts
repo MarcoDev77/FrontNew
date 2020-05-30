ef0e7b18f2a345462cc91ce52260acc7989c5b60
MauricioLoya <mauloy15@hotmail.com>
2020-05-30 14:17:49 -0500

    [UPDATE] formato de registro de visitas

--- src/app/dashboard/informatica/reporte-visitas/reporte-visitas.component.ts
+++ src/app/dashboard/informatica/reporte-visitas/reporte-visitas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InformaticaService } from '@shared/services/informatica.service';
 import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reporte-visitas',
@@ -11,9 +12,13 @@ export class ReporteVisitasComponent implements OnInit {
   public isLoading: boolean;
   public ingreso;
   public imputado: any;
+  // Filepreview
+  public file: any;
 
 
-  constructor(private informaticaService: InformaticaService) {
+  constructor(
+    private informaticaService: InformaticaService,
+    private modalService: NgbModal) {
     this.cleanForm();
   }
 
@@ -43,9 +48,50 @@ export class ReporteVisitasComponent implements OnInit {
     });
   }
 
+  generatePDF(modal) {
+    this.isLoading = true;
+    this.informaticaService.generatePDFFormatoPdfReporteVisitaCredencial(this.ingreso.id)
+      .subscribe((data: any) => {
+        this.isLoading = false;
+        this.showPreview(data, modal);
+      }, error => {
+        console.log(error);
+        this.isLoading = false;
+      });
+  }
+
   cleanForm() {
     this.ingreso = {};
     this.imputado = {};
+  }
+
+  showPreview(data, modal) {
+    const file = new Blob([data], { type: 'application/*' });
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadstart = ev => this.isLoading = true;
+    reader.onloadend = () => {
+      this.isLoading = false;
+      let dataUrl: any;
+      dataUrl = reader.result;
+      const base64 = dataUrl.split(',')[1];
+      this.modalService.dismissAll();
+      if (base64) {
+        this.file = base64;
+        this.modalService.open(modal, { size: 'lg', windowClass: 'modal-primary' });
+      }
+    };
+    reader.onerror = () => {
+      this.isLoading = false;
+      Swal.fire({
+        title: 'Error',
+        text: 'Error al generar el archivo',
+        icon: 'error',
+        timer: 1500,
+        showConfirmButton: false
+      });
+      this.modalService.dismissAll();
+    };
   }
 
 }
