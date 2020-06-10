c431c2f0028837903f0aacbdad267913f5393ebf
MauricioLoya <mauloy15@hotmail.com>
2020-06-10 13:56:13 -0500

    [FIX] Paginacion referencias, nucleo familair y bitacoras ingreso y liberacion

--- src/app/dashboard/bitacoras/bitacora-ingreso-libreracion/bitacora-ingreso-libreracion.component.ts
+++ src/app/dashboard/bitacoras/bitacora-ingreso-libreracion/bitacora-ingreso-libreracion.component.ts
@@ -39,21 +39,6 @@ export class BitacoraIngresoLibreracionComponent implements OnInit {
     this.isLoading = true;
     // TODO: el setTmeOutQuitar
     setTimeout(() => {
-      this.data = [
-        {
-          id: 1,
-          numero: 1234,
-          nombreImputado: 'Jose alvaro',
-          fechaLibertad: new Date(),
-          fecheIngreso: new Date(),
-          causaPenal: 'Delito 2',
-          juzgado: 'Juzgado 1',
-          delito: 'Robo a mano armada',
-          tipoLibertad: 'Condicional',
-          tipo: 'primodelincuente',
-          ordena: 'LIC FAUSTO',
-        }
-      ];
       this.isLoading = false;
     }, 2500);
   }
@@ -72,8 +57,8 @@ export class BitacoraIngresoLibreracionComponent implements OnInit {
   }
 
   add(modal) {
-    this.isDetails = false;
-    this.modalService.open(modal, {size: 'lg', windowClass: 'modal-primary mt-12'});
+    // this.isDetails = false;
+    // this.modalService.open(modal, {size: 'lg', windowClass: 'modal-primary mt-12'});
   }
 
   switch(e) {
