90235f7f7cb095a701e19c989bd5b8679afdc4e2
Carlos gutierrez <carlosgr822@gmail.com>
2020-06-11 13:42:27 -0500

    [WIP] beneficio estatal

--- src/app/dashboard/servicio-social/beneficio-estatal/beneficio-estatal.component.ts
+++ src/app/dashboard/servicio-social/beneficio-estatal/beneficio-estatal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ServicioSocialService } from '@shared/services/servicio-social.service';
 import Swal from 'sweetalert2';
+import { CatalogosService } from '@shared/services/catalogos.service';
 
 @Component({
   selector: 'app-beneficio-estatal',
@@ -12,17 +13,26 @@ export class BeneficioEstatalComponent implements OnInit {
   public ingreso: any
   public moreInputs: any[]
   public isLoading: boolean
-  public beneficio: any
+  public beneficio: BeneficioEstatal
+  public parentescos: []
+  public file:any
   constructor(
     private servicioSocialService: ServicioSocialService,
+    private catalogoService: CatalogosService,
     private modalService: NgbModal) { }
 
   ngOnInit() {
     this.ingreso = {} as any
     this.beneficio = {} as any
+    this.beneficio.parentesco={} as any
+    this.getParentescos()
   }
 
-
+  getParentescos() {
+    this.catalogoService.getParentescos().subscribe((data: any) => {
+      this.parentescos = data.parentescos;
+    });
+  }
   searchImputado(ingreso?) {
     console.log(ingreso)
     if (ingreso) {
@@ -46,7 +56,53 @@ export class BeneficioEstatalComponent implements OnInit {
   cleanForm() {
 
   }
+  generatePDF(modal) {
+    this.isLoading = true;
+    //TODO: Cambiar por el metodo correspondiente
+    this.servicioSocialService.generatePDFEstudioSocieconomico(this.ingreso.imputadoId)
+      .subscribe((data: any) => {
+        this.isLoading = false;
+        this.showPreview(data, modal);
+      }, error => {
+        this.isLoading = false;
+        Swal.fire({
+          title: 'Error!',
+          text: 'Error al generar el documento.',
+          icon: 'error',
+          timer: 1000,
+          showConfirmButton: false
+        });
+      });
+  }
 
+  showPreview(data, modal) {
+    const file = new Blob([data], { type: 'application/*' });
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadstart = ev => this.isLoading = true;
+    reader.onloadend = () => {
+      this.isLoading = false;
+      let dataUrl: any;
+      dataUrl = reader.result;
+      const base64 = dataUrl.split(',')[1];
+      this.modalService.dismissAll();
+      if (base64) {
+        this.file = base64;
+        this.modalService.open(modal, { size: 'lg', windowClass: 'modal-primary' });
+      }
+    };
+    reader.onerror = () => {
+      this.isLoading = false;
+      Swal.fire({
+        title: 'Error',
+        text: 'Error al generar el archivo',
+        icon: 'error',
+        timer: 1500,
+        showConfirmButton: false
+      });
+      this.modalService.dismissAll();
+    };
+  }
 
 }
 
@@ -64,6 +120,7 @@ class BeneficioEstatal {
   violenciaIntrafamiliarSecundario: Boolean
   violenciaEspecificarSecundario: String
   nivelSocioeconomicoSecundario: String
+  relacionMedioExterno: String
   familiarAntecedentesAdiccionSecundario: String
   familiarEspecifiqueSecundario: String
   familiaConductaAntisocial: String
@@ -75,4 +132,6 @@ class BeneficioEstatal {
   visitadoPersonas: Boolean
   diagnosticoPronostico: String
   opinionBeneficiosInternos: String
+  radicaEstado: boolean
+  parentesco: any
 }
