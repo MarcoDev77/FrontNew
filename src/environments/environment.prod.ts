3d76107fdabc082c98248c444be2e3e272d6a8d9
MauricioLoya <mauloy15@hotmail.com>
2020-08-17 13:12:32 -0500

    [UPDATE] ip api 167.71.251.182

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://64.227.4.101:8080/reinsercionapi',
+  // apiUrl: 'http://64.227.4.101:8080/reinsercionapi',
+  apiUrl: 'http://167.71.251.182:8080/reinsercionapi',
 };
